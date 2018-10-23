d16f4c13e6a3cb773d2f477ecd0382986c329b8e
VDSMath <matheusvdsdf@gmail.com>
2018-10-23 18:43:10 -0300

    Função pra achar soluções

--- GridToString.py
+++ GridToString.py
@@ -15,4 +15,28 @@ def SaveGrid(X):
   file.write(txt)
   currentMap += 1
   
+def FindSolutions(X):
+  auxX = [False] * 1000;
+  current = 0
+  while not(auxX[978]):
+    
+    if not(auxX[999-current]):
+      auxX[999-current] = True
+      current = 0
+      ReplaceAndCheck(X,auxX)
+    else:
+      auxX[999-current] = False
+      current = current + 1
+      
+  return
+
+def ReplaceAndCheck(X, auxX):
+  for i in range(0,1000):
+    if X[i]:
+      auxX[i] = True
+  if Sudoku(auxX):
+    print("Valido")
+    return True
+  return False
+  
     
